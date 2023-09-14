c3823a330556f8c1f4a9056b4376308a70c0bc3a
andaluciacurtis <andalucia.curtis@gmail.com>
2023-09-13 18:27:40 -0700

    Made the circle responsive + centered

--- script.js
+++ script.js
@@ -9,10 +9,22 @@ const centerContainer = document.querySelector(".center-container");
 
 collapseButtonLeft.addEventListener("click", ()=> {
   leftContainer.classList.toggle("collapsed");
+  if (leftIsOpen) {
+    collapseButtonLeft.innerHTML = `<i class="fa-solid fa-angles-right"></i>`;
+  } else {
+    collapseButtonLeft.innerHTML = `<i class="fa-solid fa-angles-left"></i>`;
+  }
+  leftIsOpen = !leftIsOpen;
 })
 
 collapseButtonRight.addEventListener("click", ()=> {
   rightContainer.classList.toggle("collapsed");
+  if (rightIsOpen) {
+    collapseButtonRight.innerHTML = `<i class="fa-solid fa-angles-left"></i>`;
+  } else {
+    collapseButtonRight.innerHTML = `<i class="fa-solid fa-angles-right"></i>`;
+  }
+  rightIsOpen = !rightIsOpen;
 })
 
 function resizeCenter() {
